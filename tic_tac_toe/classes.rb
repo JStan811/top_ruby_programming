c784998e2469fdab8e869fd024d8af7ba66f2ba9
Jake Standley <jakestandley95@gmail.com>
2021-08-27 15:32:01 -0500

    Made some progress. Game can now take each player's plays, update the board accordingly, and recognize win conditions.

--- tic_tac_toe/classes.rb
+++ tic_tac_toe/classes.rb
@@ -11,40 +11,66 @@ class Game
   end
 
   def welcome_message
-    puts 'Yo, you ready to play some Tic-Tac-Toe?'
+    puts 'Tic Tac Toe'
     puts ''
     puts "Player 1, you control the X's. Player 2, you control the Y's."
     puts ''
-    puts 'Player 1 make your play:'
   end
 end
 
 # game board object. Trying to figure out OOP, and this seems like a good thing to use as an object?
 class Board
-  attr_reader :state, :board_look
+  attr_accessor :state
 
-  @state = [{ A1: nil, A2: nil, A3: nil }, { B1: nil, B2: nil, B3: nil }, { C1: nil, C2: nil, C3: nil }]
-
-  @board_look =
-    "     _______ _______ _______
-  |       |       |       |
- A|       |       |       |
-  |_______|_______|_______|
-  |       |       |       |
- B|       |       |       |
-  |_______|_______|_______|
-  |       |       |       |
- C|       |       |       |
-  |_______|_______|_______|
-     1      2      3
-"
-
-  def set_board(player1, player2)
-    @state[0][player1] = 'X'
+  def initialize
+    @state = { A1: nil, A2: nil, A3: nil, B1: nil, B2: nil, B3: nil, C1: nil, C2: nil, C3: nil }
+    @row_wins = [{ A1: 1, A2: 1, A3: 1 }, { B1: 1, B2: 1, B3: 1 }, { C1: 1, C2: 1, C3: 1 }]
+    @column_wins = [{ A1: 1, B1: 1, C1: 1 }, { A2: 1, B2: 1, C2: 1 }, { A3: 1, B3: 1, C3: 1 }]
+    @diagonal_wins = [{ A1: 1, B2: 1, C3: 1 }, { A3: 1, B2: 1, C1: 1 }]
   end
+
+  # Win conditions as coordinates
+  # row_win_conditions_coordinates = [[1, 3], [2, 3], [3, 3]], [[1, 2], [2, 2], [3, 2]], [[1, 1], [2, 1], [3, 1]]
+  # column_win_conditions_coordinates = [[1, 1], [1, 2], [1, 3]], [[2, 1], [2, 2], [2, 3]], [[3, 1], [3, 2], [3, 3]]
+  # diagonal_win_conditions_coordinates = [[1, 3], [2, 2], [3, 1]], [[1, 1], [2, 2], [3, 3]]
 
   def show_board
-    puts @board_look
+    puts BLANK_BOARD
   end
 
+  def user_play(position, symbol)
+    position = position.to_sym
+    @state[position] = symbol
+  end
+
+  def win?(board_state)
+    rows = win_per_type?(@row_wins, board_state)
+    columns = win_per_type?(@column_wins, board_state)
+    diagonals = win_per_type?(@diagonal_wins, board_state)
+
+    rows || columns || diagonals
+  end
+
+  private
+
+  def win_per_type?(type, board_state)
+    answer = false
+
+    type.each do |hash|
+      if (hash.to_a - board_state.to_a).empty?
+        answer = true
+        break
+      end
+    end
+    answer
+  end
+end
+
+# class for a player object. Using to distinguish between P1 and P2
+class Player
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
 end
--- tic_tac_toe/script.rb
+++ tic_tac_toe/script.rb
@@ -2,13 +2,50 @@
 
 require_relative 'classes'
 
-my_board = Board.new
+# making the necessary objects. 1 game board to display and separate player    # boards to make it easier to determine win and tie
 my_game = Game.new('new')
+game_board = Board.new
+player1 = Player.new('Player 1')
+player1_board = Board.new
+player2 = Player.new('Player 2')
+player2_board = Board.new
 
 puts my_game.welcome_message
 
-puts my_board.board_look
+game_ongoing = true
 
-my_board.show_board
+while game_ongoing
+  puts "#{player1.name}, make your play:"
+  user_input = gets.chomp
+  player1_board.user_play(user_input, 1)
+  puts "#{player1.name} board: #{player1_board.state}"
+  game_board.user_play(user_input, 'X')
+  puts "Game board: #{game_board.state}"
+  puts ''
+  if player1_board.win?(player1_board.state)
+    puts "#{player1.name}, you win!"
+    break
+  end
 
-user_input = gets
+  puts "#{player2.name}, make your play:"
+  user_input = gets.chomp
+  player2_board.user_play(user_input, 1)
+  puts "#{player2.name} board: #{player2_board.state}"
+  game_board.user_play(user_input, 'Y')
+  puts "Game board: #{game_board.state}"
+  puts ''
+  if player2_board.win?(player2_board.state)
+    puts "#{player2.name}, you win!"
+    break
+  end
+end
+
+# This is how I think a turn will work
+# 1. determine player
+# 2. display board
+# 3. get player input
+# 4. update board
+# 5. check for win or tie
+#   a. if win, declare winner and exit
+#   b. if tie, declare tie and exit
+#   c. else, continue to next turn
